f4c06d04b580cc43653c4510bb57c76051454d5b
jordan cea <jordanceaeguiluz@gmail.com>
2021-05-02 21:51:34 -0400

    feature:style  added to stats

--- src/componets/interfaces/pokemonStats/PokemonStats.jsx
+++ src/componets/interfaces/pokemonStats/PokemonStats.jsx
@@ -1,14 +1,15 @@
 import { Paper, Box, Typography } from "@material-ui/core";
-import PokeStats from "./Style";
+import StyleStats from "./Style";
 
 const PokemonStats = ({ attributes }) => {
-  const classes = PokeStats();
+  const classes = StyleStats();
   return (
     <Paper className={classes.root}>
       {attributes.map((item) => (
         <Box>
-          <Typography>{item.title}</Typography>
-          <Typography>{item.attribute}</Typography>
+          <Typography className={classes.typography}>{item.title}</Typography>
+          <Typography className={classes.typography} >{item.attribute}</Typography>
+        <hr/>  
         </Box>
       ))}
     </Paper>
--- src/componets/interfaces/pokemonStats/Style.jsx
+++ src/componets/interfaces/pokemonStats/Style.jsx
@@ -1,13 +1,29 @@
 import { makeStyles } from "@material-ui/core"
 
-const PokemonStats = makeStyles({
+const StyleStats = makeStyles({
     root: {
-        backgroundColor: "#FFFFFF",
-        width : "400px",
+        background: "rgb(60,131,152)",
+        background: "linear-gradient(90deg, rgba(60,131,152,1) 10%, rgba(142,29,29,1) 100%)",
+        width : "470px",
         marginLeft: "25px",
         marginTop: "15px",
-        border: "2px solid red"
+        border: "2px solid gray",
+        borderRadius:"15px"
+
+    },
+    typography:{
+      marginLeft:"30px",
+      fontWeight:"bold",
+      color:"white",
+      padding:"5px 5px 5px 5px",
+      
+      
+      
+
+
     }
 })
 
-export default PokemonStats
+
+
+export default StyleStats
