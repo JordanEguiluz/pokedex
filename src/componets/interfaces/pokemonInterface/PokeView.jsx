24fbbe75e66582d631a8eb9451ce6414cb5f6bea
Alex <alexbastiansilvafigueroa@gmail.com>
2021-08-01 21:50:39 -0400

    feat: add button show image

--- src/componets/interfaces/pokemonInterface/PokeView.jsx
+++ src/componets/interfaces/pokemonInterface/PokeView.jsx
@@ -1,15 +1,26 @@
-import React from "react"
-import {Typography, Container } from "@material-ui/core"
+import React, { useState } from "react"
+import {Typography, Container, Button } from "@material-ui/core"
 import PokeImg from "../../../assets/img/charmander.png"
 import styleOverride  from "./Style"
 
-
 const PokemonView = () => {
     const classes = styleOverride()
+    const [ isVisible, isSetVisible ] = useState(true)
+    console.log(isVisible)
+
+    const handleImage = () => {
+        isSetVisible(!isVisible)
+    }
+    
     return (
+        <>
+        <Button  onClick={handleImage} style={{backgroundColor: 'gray', color: '#FFFFFF'}}>{isVisible ? 'Ocultar' :' Mostrar'}</Button>
+        { isVisible ?
         <Container>
             <img src={PokeImg} alt="Img" className={classes.root} />
-        </Container>
+        </Container> : 
+        <h1>Imagen oculta</h1>}
+        </>
     )
 }
 
