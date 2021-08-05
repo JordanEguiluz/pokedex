cd380efabfa1bda55824784c0ff85f6aa9eb0b09
JordanEguiluz <mugetsu1996@gmail.com>
2021-08-05 18:14:30 -0400

    feat: add  botton reset count

--- src/componets/interfaces/pokemonInterface/PokeView.jsx
+++ src/componets/interfaces/pokemonInterface/PokeView.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, Component } from "react"
 import {Typography, Container, Button,IconButton } from "@material-ui/core"
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import PokeImg from "../../../assets/img/charmander.png"
@@ -20,23 +20,29 @@ const PokemonView = () => {
     const handleLike = () => {
         isSetLike(isLike +1 )
     }
+    const handleReset = () => {
+         isSetLike(0 )
+}
     
-   
+
+
     return (
         <>
-
         <Button  onClick={handleImage} style={{backgroundColor: 'gray', color: '#FFFFFF'}}>{isVisible ? 'Ocultar' :' Mostrar'}</Button>
-        {}
-        { isLike  }
-        <Button onClick={handleLike}><ThumbUpAltIcon  style={{backgroundColor: 'gray', color: '#5882FA'}}></ThumbUpAltIcon></Button>
-´
-        <Button onClick={handleLike}> {isLike ? ' Reiniciar' : '  '}</Button>
-    
          { isVisible ?
         <Container>
             <img src={PokeImg} alt="Img" className={classes.root} />
         </Container> : 
-        <h1>Imagen oculta</h1>}
+        <h2>Imagen oculta</h2>}
+       
+         <Button
+          onClick={handleLike}><ThumbUpAltIcon  style={{backgroundColor: 'gray', color: '#5882FA'}}></ThumbUpAltIcon>
+         </Button>
+         { isLike  }
+         <Button
+          onClick={handleReset  } style={{backgroundColor: 'gray', color: '#FFFFFF'}}> {isLike?  'Reiniciar': 'Reiniciado'}
+          </Button>
+         
         </>
     )
 
