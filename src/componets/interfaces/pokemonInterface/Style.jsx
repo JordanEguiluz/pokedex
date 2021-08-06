aad146beb10881280ec05430e360a4f8af38af8e
JordanEguiluz <mugetsu1996@gmail.com>
2021-08-06 03:57:47 -0400

    feature:update style css

--- src/componets/interfaces/pokemonInterface/Style.jsx
+++ src/componets/interfaces/pokemonInterface/Style.jsx
@@ -3,9 +3,19 @@ import { makeStyles } from "@material-ui/core"
 const styleOverride = makeStyles({
     root: {
         backgroundColor:"#464646",
-        marginTop:"100px"
-    }
+        marginTop:"10px",
+        width : "470px",
+        height:"400px",
+           
+    },
 
+    cajaFoto:{
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"flex-start",
+        position:"absolute",  
+        margin:"10px"      
+    }
 
 } )
 
--- src/componets/interfaces/pokemonStats/Style.jsx
+++ src/componets/interfaces/pokemonStats/Style.jsx
@@ -1,15 +1,19 @@
 import { makeStyles } from "@material-ui/core"
 
+
 const StyleStats = makeStyles({
     root: {
-        background: "rgb(60,131,152)",
+        
         background: "linear-gradient(90deg, rgba(60,131,152,1) 10%, rgba(142,29,29,1) 100%)",
         width : "470px",
-        marginLeft: "25px",
+        height:"400px",
+        marginRigth: "2000px",
         marginTop: "15px",
         border: "2px solid gray",
-        borderRadius:"15px"
-
+        borderRadius:"15px",
+            
+        
+      
     },
     typography:{
       marginLeft:"30px",
@@ -17,13 +21,22 @@ const StyleStats = makeStyles({
       color:"white",
       padding:"5px 5px 5px 5px",
       
+      },
+  caja:{
+    display:"flex",
+     flexDirection:"row-reverse",
+     justifyContent:"space-around",
+     alignItems:"baseline",
+     
+       
+     
+
+
+}
+
+    })
+
+  
+
       
-      
-
-
-    }
-})
-
-
-
-export default StyleStats
+export default  StyleStats
