0fb05d40b523e83d34d7c43f9073cd2f5407574a
Alex Silva Figueroa <alexbastiansilvafigueroa@gmail.com>
2021-08-28 19:59:23 -0400

    chore: add code style and formart, ts to project and atomic desing structure

---
+++ src/presentation/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => (
+  <div className="App">
+    <h1>Pokedex</h1>
+    {/* <PokemonView />
+    <PokemonStats attributes={Data} /> */}
+  </div>
+)
+
+export default App
